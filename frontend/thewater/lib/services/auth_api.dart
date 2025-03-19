18696dc85700647313efa5c299d47f0712cee52e
LeeGukgeon <lkukun0223@gmail.com>
2025-03-19 15:40:29 +0900

    Feat : email check api

--- frontend/thewater/lib/services/auth_api.dart
+++ frontend/thewater/lib/services/auth_api.dart
@@ -1,23 +1,45 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 const String baseUrl = 'https://j12c201.p.ssafy.io/api';
 
 class AuthApi {
-  Future<Map<String, dynamic>> checkIdAvailability(String loginId) async {
+  Future<bool> checkIdAvailability(String loginId) async {
     final url = Uri.parse(
       '$baseUrl/users/check-id?login_id=$loginId',
     ); //Uri 객체로 사용하는 이유는 URL 을 안전하게 다룰 수 있는 여러 기능을 제공하기 때문이다.
     try {
       final response = await http.get(url);
+      final json = jsonDecode(response.body);
       if (response.statusCode == 200 || response.statusCode == 409) {
-        return jsonDecode(response.body);
+        debugPrint("이메일 중복체크 완료");
+        return json['available'];
       } else {
-        return {'available': false, 'message': '이미 사용중인 아이디입니다.'};
+        return false;
       }
     } catch (e) {
-      print(e);
-      return {'available': false, 'message': '이미 사용중인 아이디입니다.'};
+      debugPrint("이메일 중복 체크 중 오류 발생");
+      return true;
+    }
+  }
+
+  Future<List<dynamic>> getFishList(String userId) async {
+    final url = Uri.parse('$baseUrl/fish');
+    final headers = <String, String>{};
+    final body = jsonEncode({
+      'kakaoAccessToken': 'kakaoAccessToken',
+      'fcmNotificationToken': 'fcmToken',
+    });
+    final response = await http.post(url, headers: headers, body: body);
+    final json = jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      debugPrint("물고기 목록 조회 성공");
+
+      return json['data'];
+    } else {
+      debugPrint("물고기 목록 조회 실패! json: $json");
+      throw Exception('lib/services/auth_api.dart');
     }
   }
 }
