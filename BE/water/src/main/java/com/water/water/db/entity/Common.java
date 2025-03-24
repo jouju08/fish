e3b56d250ab8677979fa4cafee8468f78c07c2c4
jouju08 <juha0824@gmail.com>
2025-03-24 13:16:16 +0900

    Refactor: replace old entity files with new ones

---
+++ BE/water/src/main/java/com/water/water/db/entity/Common.java
@@ -0,0 +1,37 @@
+package com.water.water.db.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Data
+@ToString
+@MappedSuperclass
+public abstract class Common {
+
+    @Column(name="created_at", updatable = false)
+    @CreatedDate
+    private String createdAt;
+
+    @Column(name="updated_at")
+    @LastModifiedDate
+    private String updatedAt;
+
+    @PrePersist
+    public void onPrePersist() {
+        this.createdAt = LocalDateTime.now().toString();
+    }
+
+    @PreUpdate
+    public void onPreUpdate() {
+        this.updatedAt = LocalDateTime.now().toString();
+    }
+}
