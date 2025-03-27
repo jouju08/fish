0925577dab9c9e8fe1f0d66d4f907614f443e923
이수정 <sj.prog.hub@gmail.com>
2025-03-27 16:21:50 +0900

    Feat: 어항 좋아요 api 추가

--- BE/thewater/src/main/java/fishermanjoeandchildren/thewater/controller/AquariumController.java
+++ BE/thewater/src/main/java/fishermanjoeandchildren/thewater/controller/AquariumController.java
@@ -4,6 +4,7 @@ import fishermanjoeandchildren.thewater.data.ResponseMessage;
 import fishermanjoeandchildren.thewater.data.ResponseStatus;
 import fishermanjoeandchildren.thewater.data.dto.ApiResponse;
 import fishermanjoeandchildren.thewater.data.dto.AquariumDto;
+import fishermanjoeandchildren.thewater.db.entity.Aquarium;
 import fishermanjoeandchildren.thewater.security.JwtUtil;
 import fishermanjoeandchildren.thewater.service.AquariumService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -60,6 +61,74 @@ public class AquariumController {
                         .build();
         }
     }
+
+    @SecurityRequirement(name="BearerAuth")
+    @PostMapping(value="/like/{aquarium_id}")
+    public ApiResponse<?> likeAquarium(@PathVariable("aquarium_id") Long aquariumId, HttpServletRequest request){
+        String token = jwtUtil.resolveToken(request);
+        Long memberId = jwtUtil.extractUserId(token);
+
+        String status = aquariumService.likeAquarium(aquariumId,memberId);
+
+        switch(status){
+            case ResponseStatus.SUCCESS:
+                return ApiResponse.builder()
+                        .status(status)
+                        .message(ResponseMessage.SUCCESS)
+                        .data("어항에 좋아요를 눌렀습니다.")
+                        .build();
+            case ResponseStatus.NOT_FOUND:
+                return ApiResponse.builder()
+                        .status(status)
+                        .message(ResponseMessage.NOT_FOUND)
+                        .data("해당 어항을 찾을 수 없습니다.")
+                        .build();
+            case ResponseStatus.CONFLICT:
+                return ApiResponse.builder()
+                        .status(status)
+                        .message(ResponseMessage.CONFLICT)
+                        .data("이미 좋아요를 누른 어항입니다.")
+                        .build();
+            default:
+                return ApiResponse.builder()
+                        .status(status)
+                        .message(ResponseMessage.SERVER_ERROR)
+                        .data("어항 좋아요 처리중 오류가 발생했습니다.")
+                        .build();
+        }
+
+    }
+
+    @SecurityRequirement(name="BearerAuth")
+    @DeleteMapping("/like/{aquarium_id}")
+    public ApiResponse<?> unlikeAquarium(@PathVariable("aquarium_id") Long aquariumId, HttpServletRequest request){
+        String token = jwtUtil.resolveToken(request);
+        Long memberId = jwtUtil.extractUserId(token);
+
+        String status = aquariumService.unlikeAquarium(aquariumId, memberId);
+
+        switch(status){
+            case ResponseStatus.SUCCESS:
+                return ApiResponse.builder()
+                        .status(status)
+                        .message(ResponseMessage.SUCCESS)
+                        .data("좋아요가 취소되었습니다.")
+                        .build();
+            case ResponseStatus.NOT_FOUND:
+                return ApiResponse.builder()
+                        .status(status)
+                        .message(ResponseMessage.NOT_FOUND)
+                        .data("좋아요 기록이 없습니다.")
+                        .build();
+            default:
+                return ApiResponse.builder()
+                        .status(status)
+                        .message(ResponseMessage.SERVER_ERROR)
+                        .data("좋아요 취소 중 오류가 발생했습니다.")
+                        .build();
+        }
+    }
+
 
 
 
