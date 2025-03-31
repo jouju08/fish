42cd7c58f76e085f348d0cfcbf61996de2c21e68
이수정 <sj.prog.hub@gmail.com>
2025-03-31 17:34:17 +0900

    Feat: 물고기 도감 물고기 추가 제거 받아오기

--- BE/thewater/src/main/java/fishermanjoeandchildren/thewater/controller/FishCardController.java
+++ BE/thewater/src/main/java/fishermanjoeandchildren/thewater/controller/FishCardController.java
@@ -15,12 +15,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/")
+@RequestMapping("/api/collection")
 @RequiredArgsConstructor
 public class FishCardController {
     private final FishCardService fishCardService;
     private final JwtUtil jwtUtil;
-    @GetMapping("myfish/all")
+    @GetMapping("/myfish/all")
     public ApiResponse<List<FishCardDto>>getMyFishAll(HttpServletRequest request) {
         String token = jwtUtil.resolveToken(request);
         Long memberId = jwtUtil.extractUserId(token);
@@ -29,15 +29,46 @@ public class FishCardController {
                 .data(allFishCards).build();
     }
 
-    @PostMapping("collection/add")
-    public ApiResponse<FishCard> addFishCard(@RequestBody FishCardDto fishCardDto, HttpServletRequest request) {
+
+    @PostMapping("/myfish/add")
+    public ApiResponse<FishCardDto> addFishCard(@RequestBody FishCardDto fishCardDto, HttpServletRequest request) {
         String token = jwtUtil.resolveToken(request);
         Long memberId = jwtUtil.extractUserId(token);
-        FishCard fishCard=fishCardService.addFishCard(fishCardDto, memberId);
-        return ApiResponse.<FishCard>builder()
-                .data(fishCard)
+        FishCardDto savedFishCardDto =fishCardService.addFishCard(fishCardDto,memberId);
+        return ApiResponse.<FishCardDto>builder()
+                .data(savedFishCardDto)
                 .message(ResponseMessage.SUCCESS)
                 .status(ResponseStatus.SUCCESS)
                 .build();
     }
+
+    @DeleteMapping("/myfish/delete/{fishcard_id}")
+    public ApiResponse<?> deleteFishCard(@PathVariable("fishcard_id") Long fishCardId, HttpServletRequest request) {
+        String token = jwtUtil.resolveToken(request);
+        Long memberId = jwtUtil.extractUserId(token);
+
+        String status = fishCardService.deleteFishCard(fishCardId, memberId);
+
+        switch (status){
+            case ResponseStatus.SUCCESS:
+                return ApiResponse.builder()
+                        .status(status)
+                        .message(ResponseMessage.SUCCESS)
+                        .data("물고기 카드 삭제 완료")
+                        .build();
+            case ResponseStatus.NOT_FOUND:
+                return ApiResponse.builder()
+                        .status(status)
+                        .message(ResponseMessage.NOT_FOUND)
+                        .data("삭제할 물고기 카드를 찾을 수 없습니다.")
+                        .build();
+            default:
+                return ApiResponse.builder()
+                        .status(status)
+                        .message(ResponseMessage.SERVER_ERROR)
+                        .data("물고기 카드 삭제 중 오류가 발생했습니다.")
+                        .build();
+        }
+    }
+
 }
--- BE/thewater/src/main/java/fishermanjoeandchildren/thewater/db/repository/FishCardRepository.java
+++ BE/thewater/src/main/java/fishermanjoeandchildren/thewater/db/repository/FishCardRepository.java
@@ -4,13 +4,16 @@ import fishermanjoeandchildren.thewater.data.dto.FishCardDto;
 import fishermanjoeandchildren.thewater.db.entity.FishCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface FishCardRepository extends JpaRepository<FishCard, Long> {
-    @Query("SELECT fc FROM FishCard fc where fc.hasDeleted=false AND fc.member=:memberId")
-    List<FishCardDto>findFishCardExceptDeleted(Long memberId);
+    @Query("SELECT f FROM FishCard f WHERE f.member.id = :memberId AND f.hasDeleted = false")
+    List<FishCard> findFishCardExceptDeleted(@Param("memberId") Long memberId);
+
+
 
 }
