a9de3e829fa5a88158334fab249a050351a91696
이수정 <sj.prog.hub@gmail.com>
2025-04-01 16:43:00 +0900

    Feat:물고기 이미지 요청 api

--- BE/thewater/src/main/java/fishermanjoeandchildren/thewater/controller/FishCardController.java
+++ BE/thewater/src/main/java/fishermanjoeandchildren/thewater/controller/FishCardController.java
@@ -1,22 +1,26 @@
 package fishermanjoeandchildren.thewater.controller;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import fishermanjoeandchildren.thewater.data.ResponseMessage;
 import fishermanjoeandchildren.thewater.data.ResponseStatus;
 import fishermanjoeandchildren.thewater.data.dto.ApiResponse;
 import fishermanjoeandchildren.thewater.data.dto.FishCardDto;
-import fishermanjoeandchildren.thewater.db.entity.FishCard;
 import fishermanjoeandchildren.thewater.security.JwtUtil;
 import fishermanjoeandchildren.thewater.service.FishCardService;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
@@ -25,6 +29,10 @@ import java.util.List;
 public class FishCardController {
     private final FishCardService fishCardService;
     private final JwtUtil jwtUtil;
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
     @GetMapping("/myfish/all")
     public ApiResponse<List<FishCardDto>>getMyFishAll(HttpServletRequest request) {
         String token = jwtUtil.resolveToken(request);
@@ -80,5 +88,25 @@ public class FishCardController {
                         .build();
         }
     }
+
+    @GetMapping("/myfish/image/{filename}")
+    public ResponseEntity<Resource> getImage(@PathVariable String filename) throws IOException {
+        Path imagePath = Paths.get(uploadDir).resolve(filename).normalize();
+        Resource resource = new UrlResource(imagePath.toUri());
+
+        if (resource.exists() && resource.isReadable()) {
+            String contentType = Files.probeContentType(imagePath);
+            if (contentType == null) {
+                contentType = "application/octet-stream";
+            }
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .body(resource);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 
 }
