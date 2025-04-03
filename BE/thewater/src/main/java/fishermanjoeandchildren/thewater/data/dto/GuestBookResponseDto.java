096dba3799804d75117dffaeda982c7f69e333d8
이수정 <sj.prog.hub@gmail.com>
2025-04-03 14:36:10 +0900

    Refactor:방명록 requestbody 편리하게 수정

--- BE/thewater/src/main/java/fishermanjoeandchildren/thewater/data/dto/GuestBookResponseDto.java
+++ BE/thewater/src/main/java/fishermanjoeandchildren/thewater/data/dto/GuestBookResponseDto.java
@@ -3,38 +3,31 @@ package fishermanjoeandchildren.thewater.data.dto;
 import fishermanjoeandchildren.thewater.db.entity.Aquarium;
 import fishermanjoeandchildren.thewater.db.entity.GuestBook;
 import fishermanjoeandchildren.thewater.db.entity.Member;
-import fishermanjoeandchildren.thewater.db.repository.AquariumRepository;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-import jdk.jfr.Description;
 import lombok.*;
 
-@Schema(description = "방명록 정보")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @Builder
-public class GuestBookDto {
-
-    @Schema(description = "get요청시 사용되는 부분, post시 해당 값 보내지 않아도 됨")
+public class GuestBookResponseDto {
     private Long guestBookId;
 
     @NotNull
-    @Schema(description = "get요청시 사용되는 부분, post시 해당 값 보내지 않아도 됨")
     private String guestNickname;
 
     @NotNull
     private String guestBookComment;
 
-    @Schema(description = "get요청시 사용되는 부분, post시 해당 값 보내지 않아도 됨")
     private Boolean wroteByMe;
 
-    public static GuestBookDto fromEntity(GuestBook guestBook, Long currentMember){
+    public static GuestBookResponseDto fromEntity(GuestBook guestBook, Long currentMember){
 
         boolean myCommnet = guestBook.getId() == currentMember;
 
-        return GuestBookDto.builder()
+        return GuestBookResponseDto.builder()
                 .guestBookId(guestBook.getId())
                 .guestNickname(guestBook.getGuest().getNickname())
                 .guestBookComment(guestBook.getComment())
@@ -42,7 +35,7 @@ public class GuestBookDto {
                 .build();
     }
 
-    public static GuestBook toEntity(GuestBookDto guestBookDto, Aquarium aquarium , Member member){
+    public static GuestBook toEntity(GuestBookResponseDto guestBookDto, Aquarium aquarium , Member member){
 
         return GuestBook.builder()
                 .comment(guestBookDto.getGuestBookComment())
@@ -50,5 +43,4 @@ public class GuestBookDto {
                 .guest(member)
                 .build();
     }
-
 }
