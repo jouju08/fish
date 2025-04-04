14a6eb8476e2cac8c6865d5e32593a1be3093b60
황치운 <hcun1004@naver.com>
2025-04-04 12:03:15 +0900

    Feat: 사용자 찾기 기능 추가

--- BE/thewater/src/main/java/fishermanjoeandchildren/thewater/db/repository/MemberRepository.java
+++ BE/thewater/src/main/java/fishermanjoeandchildren/thewater/db/repository/MemberRepository.java
@@ -2,7 +2,10 @@ package fishermanjoeandchildren.thewater.db.repository;
 
 import fishermanjoeandchildren.thewater.db.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +14,9 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
     Optional<Member> findByLoginId(String loginId);
+
+    List<Member> findByNicknameContaining(String nickname);
+
+    @Query("SELECT m.nickname FROM Member m WHERE m.has_deleted = false")
+    List<String> findAllActiveNicknames();
 }
