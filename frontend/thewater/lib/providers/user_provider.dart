6a2ea76773e7f8e977c3dec2b87cdfc9d7d9012c
LeeGukgeon <lkukun0223@gmail.com>
2025-04-04 14:37:52 +0900

    feat : fish point detail start

--- frontend/thewater/lib/providers/user_provider.dart
+++ frontend/thewater/lib/providers/user_provider.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
-const String baseUrl = 'http://j12c201.p.ssafy.io:8081/api';
+const String baseUrl = 'http://j12c201.p.ssafy.io/api';
 
 class UserModel extends ChangeNotifier {
   final _storage = const FlutterSecureStorage();
@@ -30,18 +30,24 @@ class UserModel extends ChangeNotifier {
   }
 
   /// 로그인
-  Future<void> login(String loginId, String password) async {
-    final url = Uri.parse('$baseUrl/users/login');
-    final headers = {'Content-Type': 'application/json'};
-    final body = jsonEncode({"loginId": loginId, "password": password});
+  Future<bool> login(String loginId, String password) async {
+    try {
+      final url = Uri.parse('$baseUrl/users/login');
+      final headers = {'Content-Type': 'application/json'};
+      final body = jsonEncode({"loginId": loginId, "password": password});
 
-    final response = await http.post(url, headers: headers, body: body);
-    final decodedBody = jsonDecode(utf8.decode(response.bodyBytes));
+      final response = await http.post(url, headers: headers, body: body);
+      final decodedBody = jsonDecode(utf8.decode(response.bodyBytes));
 
-    await _storage.write(key: 'token', value: decodedBody['data']['token']);
-    await fetchUserInfo(); // fetchUserInfo가 완료될 때까지 기다림
+      await _storage.write(key: 'token', value: decodedBody['data']['token']);
+      await fetchUserInfo(); // fetchUserInfo가 완료될 때까지 기다림
 
-    notifyListeners(); // 상태 변경을 알림
+      notifyListeners(); // 상태 변경을 알림
+      return decodedBody['data']['success'];
+    } catch (e) {
+      debugPrint("login() 오류: $e");
+      return false; // 로그인 실패 시 false 반환
+    }
   }
 
   /// 사용자 정보 가져오기
