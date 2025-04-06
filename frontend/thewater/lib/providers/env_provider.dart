7a1e820fde776d414e630709c1232ebe4604e7dc
jaemin-hand <thswoalsrns@naver.com>
2025-04-06 18:39:46 +0900

    Feat:ranking

--- frontend/thewater/lib/providers/env_provider.dart
+++ frontend/thewater/lib/providers/env_provider.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
-import 'dart:io';
-import 'package:dio/dio.dart';
 
 const String baseUrl = 'http://j12c201.p.ssafy.io';
 
