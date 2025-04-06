68510bacff35ba6439199f75c1b663a9b5a0dfad
jaemin-hand <thswoalsrns@naver.com>
2025-04-06 22:57:35 +0900

    Feat:friendAquarium

---
+++ frontend/thewater/lib/providers/guestbook_provider.dart
@@ -0,0 +1,13 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+// import 'dart:convert';
+// import 'package:http/http.dart' as http;
+// import 'package:dio/dio.dart';
+// import 'dart:io';
+
+// const String baseUrl = 'http://j12c201.p.ssafy.io/api';
+
+// class GuestBookModel extends ChangeNotifier {
+//   final _storage = const FlutterSecureStorage();
+  
+// }
