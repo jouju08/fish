c5f7ce4f998721047b8aa12cc91e17819cbc5318
이수정 <sj.prog.hub@gmail.com>
2025-04-10 17:15:16 +0900

    Feat: 아쿠아리움 물고기 정보 추가

--- BE/thewater/src/main/java/fishermanjoeandchildren/thewater/data/dto/AquariumFishCardDto.java
+++ BE/thewater/src/main/java/fishermanjoeandchildren/thewater/data/dto/AquariumFishCardDto.java
@@ -3,6 +3,8 @@ package fishermanjoeandchildren.thewater.data.dto;
 import fishermanjoeandchildren.thewater.db.entity.FishCard;
 import lombok.*;
 
+import java.time.LocalDate;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,13 +13,16 @@ import lombok.*;
 public class AquariumFishCardDto {
     private String fishName;
     private Double fishSize;
-    private String fishingPointName;
+    private Double waterTemperature;
+    private LocalDate collectDate;
 
     //TODO 잡은 날짜
     public static AquariumFishCardDto fromEntity(FishCard fishCard) {
         return AquariumFishCardDto.builder()
                 .fishName(fishCard.getFish().getFishName())
                 .fishSize(fishCard.getFishSize())
+                .waterTemperature(fishCard.getWaterTemperature())
+                .collectDate(fishCard.getCollectDate())
                 .build();
     }
 }
